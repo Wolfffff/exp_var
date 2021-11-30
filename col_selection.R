74714cbb516ec97aed2b23f426fd63de86206315
Diogo Melo <diogro@gmail.com>
2021-11-30 10:54:40 -0500

    crap cols selection

--- col_selection.R
+++ col_selection.R
@@ -1,11 +1,16 @@
 names(exp_data_rc3)
 
-metadata <- colData(exp_data_rc3[["ERP107748"]])
-mask_no_levels = sapply(metadata, function(x) length(unique(x)) > 1)
-names(metadata)[mask_no_levels]
-head(metadata[,mask_no_levels])
+crap_cols = c("alias", "Alias", "Broker.name", "broker.name", "Description", "Title", "ENA.checklist", "ENA.FIRST.PUBLIC", "ENA.LAST.UPDATE", "isolate", "INSDC.center.alias", 
+"INSDC.center.name", "INSDC.first.public", "INSDC.last.update", "INSDC.status", "Sample.Name", "SRA.accession", "title")
 
-col = metadata[["title"]]
+metadata <- colData(exp_data_rc3[["ERP017126"]])
+mask_no_levels = sapply(metadata, function(x) length(unique(x)) <= 1)
+mask_crap_cols = names(metadata) %in% crap_cols
+mask = unlist(Map(`|`, mask_no_levels, mask_crap_cols))
+names(metadata)[!mask]
+head(metadata[,!mask])
+
+col = metadata[["cell.gating"]]
 length(col)
 head(col)
 length(unique(col))
