93cb80361f5d305d3f5c63ba1dd83837b02e7d60
Diogo Melo <diogro@gmail.com>
2021-12-20 15:02:42 -0500

    permission changes and some new plots

--- runRecount.R
+++ runRecount.R
@@ -47,13 +47,14 @@ results_list_rc3 <- llply(names(exp_data_rc3),
 names(results_list_rc3) <- names(exp_data_rc3)
 
 names(results_list_rc3)[(is.na(results_list_rc3))]
-# re_run = "BLCA"
+# re_run = "SRP216404"
+# dset_name = re_run
 # results_list_rc3[[re_run]] <- main_loop(dset_name = re_run, exp_data = exp_data_rc3,
 #                       experimental_metadata = experimental_metadata_rc3,
 #                       feature_vec = feature_vec,
 #                       assay_name = "raw_counts")
 
-#save(results_list_rc3, file = "Rdatas/results_list_rc3.RData")
+save(results_list_rc3, file = "Rdatas/results_list_rc3.RData")
 #load("Rdatas/results_list_rc3.RData")
 for (dset_name in names(results_list_rc3)) {
     if(!is.na(results_list_rc3[[dset_name]]))
@@ -62,3 +63,22 @@ for (dset_name in names(results_list_rc3)) {
                   base_height = 6, base_asp = 1.2, ncol = 2, nrow = 2)
 }
 
+variance_df_list = vector("list", length = length(results_list_rc3))
+names(variance_df_list) = names(results_list_rc3)
+for (dset_name in names(results_list_rc3)) {
+  print(dset_name)
+  exprDf = results_list_rc3[[dset_name]]
+  gene_vars = rowVars(exprDf$residuals_noOut)
+  variance_df_list[[dset_name]] = data.frame(
+      Genes = names(gene_vars),
+      vars = gene_vars)
+  }
+variance_df = reduce(variance_df_list, inner_join, by = "Genes")
+colnames(variance_df)[-1] = names(results_list_rc3)
+
+pak::pkg_install("corrplot")
+library(corrplot)
+var_cor = cor(variance_df[,-1], method = "s")
+png("data/var_corr_plot_rc3.png", height = 1080, width = 1080)
+corrplot.mixed(var_cor, upper = "ellipse")
+dev.off()
