3f4c2b981f7309207e0558ab7dfe6e26afe958fc
Wolfffff <scott.w.wolf1@gmail.com>
2022-01-19 15:02:10 -0500

    Cleaning and adding generic row-wise metrics function

---
+++ snakemake_processing.R
@@ -0,0 +1,24 @@
+# rc3 input 
+source("functions.R")
+library(here)
+
+file_paths <- list.files(path = here("snakemake/Rdatas/residuals/"), pattern = "\\.rds", full.names = TRUE)
+file_names <-  gsub(pattern = "\\.rds$", replacement = "", x = basename(file_paths))
+
+data_list <- lapply(file_paths, readRDS)
+
+names(data_list) <- file_names  
+
+results_list_rc3
+
+# Process row wise metrics
+library(dplyr)
+library(Rfast)
+library(matrixStats)
+
+
+row_means_df = calculate_row_wise_metric(results_list_rc3,rowMeans)
+row_var_df = calculate_row_wise_metric(results_list_rc3,rowVars)
+row_sd_df = calculate_row_wise_metric(results_list_rc3,rowSds)
+row_cv_df = calculate_row_wise_metric(results_list_rc3,rowcvs)
+
