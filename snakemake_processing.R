b0784285d37274ba92282cacfc502c566468b49c
Diogo Melo <diogro@gmail.com>
2022-01-21 11:27:16 -0500

    parallel read rds

--- snakemake_processing.R
+++ snakemake_processing.R
@@ -5,7 +5,10 @@ library(here)
 file_paths <- list.files(path = here("snakemake/Rdatas/residuals/"), pattern = "\\.rds", full.names = TRUE)
 file_names <-  gsub(pattern = "\\.rds$", replacement = "", x = basename(file_paths))
 
-data_list <- lapply(file_paths, readRDS)
+library(plyr)
+library(doMC)
+registerDoMC(64)
+data_list <- llply(file_paths, readRDS, .parallel = TRUE)
 
 names(data_list) <- file_names  
 
