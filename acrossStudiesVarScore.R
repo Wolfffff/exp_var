b0713a68eb6481b98a338acda7870c7a7683368d
Diogo Melo <diogro@gmail.com>
2022-01-27 11:59:01 -0500

    PCoA plots

--- acrossStudiesVarScore.R
+++ acrossStudiesVarScore.R
@@ -5,6 +5,8 @@ metric_df = readRDS(here::here("snakemake/Rdatas/gene_metrics.RDS"))
 rank_list = list()
 pak::pkg_install("corrplot")
 library(corrplot)
+library(vegan)
+library(ape)
 
 for (metric in c("means", "var", "sd", "cv")){
     print(metric)
@@ -18,8 +20,14 @@ for (metric in c("means", "var", "sd", "cv")){
     corrplot.mixed(metric_cor, upper = "ellipse")
     dev.off()
 
-    PC_scores = as.matrix(rank_mat) %*% eigen(metric_cor)$vectors
-    
+    res <- pcoa(abs(1 - metric_cor))
+    png(here::here(paste0("data/plots/SpearmanCorrelations/",metric,"_PCoA_plot.png")), height = 1080, width = 1080)
+    biplot(res)
+    dev.off()
+
+    eig = eigen(metric_cor)
+    PC_scores = as.matrix(rank_mat) %*% eig$vectors
+    print(paste(round(eig$values/sum(eig$values) * 100, 1)[1:5], collapse = "% "))
     gene_rank = rank(PC_scores[,1])
     names(gene_rank) = metric_df[[metric]][,1]
     rank_list[[metric]] = gene_rank
@@ -29,13 +37,8 @@ rank_df = data.frame(bind_cols(rank_list))
 rank_df$gene = metric_df[[1]][,1]
 write.csv(rank_df, file=here::here("data/pca_ranks.csv"))
 
-library(vegan)
-library(ape)
-res <- pcoa(abs(1 - metric_cor))
-res$values
-png("../test.png")
-biplot(res)
-dev.off()
+
+
 
 #  Modularity
 pak::pkg_install("diogro/evolqg")
@@ -44,7 +47,7 @@ library(evolqg)
 library(yamdar)
 
 groups = c(rep("EA", 8), rep("gTEX", 17),  rep("tcga", 12), rep("misc", 4))
-names(groups) =  colnames(var_cor)
+names(groups) =  colnames(metric_cor)
 toHypotMatrix(groups)
 
 TestModularity(var_cor, toHypotMatrix(groups), permutations = 10000)
