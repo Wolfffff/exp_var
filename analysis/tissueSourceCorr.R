139cae61c4ffd293b02d42dea7a57b78597a1462
Diogo Melo <diogro@gmail.com>
2022-02-23 14:24:07 -0500

    legend

--- analysis/tissueSourceCorr.R
+++ analysis/tissueSourceCorr.R
@@ -124,6 +124,6 @@ p_study = mcmc_intervals(fit$draws("as")) +
 
 p_model = p_study + (p_tissue / p_source)  +
   plot_annotation(title = "Modeling the driver of across-study variance correlations",
-    caption = "Linear effect model coefficients with Fisher z-transformed spearman correlations as the response. \n A: The pairwise random effect captures the non-independence of the correlation values and estimates \n the contribution of each study to the correlation. For example: comparisons involving bone_marrow \n tend to be lower than the others. B and C: Fixed effect estimates: correlations among studies that use \n the same tissue are higher, and correlations involving studies in the \"Other\" category (non gTEX and TCGA) \n tend to be lower.")
+    caption = "Linear effect model coefficients with Fisher z-transformed spearman correlations as the response. \n A: The pairwise random effect captures the non-independence of the correlation values and estimates the contribution \n of each study to the correlation. For example: comparisons involving bone_marrow tend to be lower than the others. \n B and C: Fixed effect estimates: correlations among studies that use the same tissue are higher, and correlations \ninvolving studies in the \"Other\" category (non gTEX and TCGA) tend to be lower.")
 save_plot(here::here("data/plots/correlationModeling.png"),
           p_model, base_height = 6, base_asp = 1, ncol = 2, nrow = 2)
