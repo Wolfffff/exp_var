d0876fad49daa4da4c5273d4b29bd7c7b2cf07af
Diogo Melo <diogro@gmail.com>
2022-02-24 13:43:59 -0500

    some style and comment char options

--- analysis/violin_plots.R
+++ analysis/violin_plots.R
@@ -1,14 +1,19 @@
 source(here::here("functions.R"))
 
 metric_df = readRDS(here::here("snakemake/Rdatas/gene_metrics.RDS"))
-ea_df = read.csv(here::here("snakemake/metadata/EA_metadata.csv"), header=T)
-rc3_df = read.csv(here::here("snakemake/metadata/recount3_metadata.csv"), header=T)
+ea_df = read.csv(here::here("snakemake/metadata/EA_metadata.csv"),
+                 header=T, comment.char = "#")
+rc3_df = read.csv(here::here("snakemake/metadata/recount3_metadata.csv"),
+                  header=T, comment.char = "#")
 metadata_df = bind_rows(ea_df,rc3_df)
 rank_df = read.csv(here::here("data/pca_ranks.csv"), header = TRUE)[, -1]
 
 
 
-quantile_violin_plot(rank_df$sd,rank_df$mean) + ylab("SD Rank") + xlab("metric quantile") + geom_boxplot(width=0.1)# + theme_minimal() + theme(legend.position = "none")
+quantile_violin_plot(rank_df$sd,rank_df$mean) + 
+    ylab("SD Rank") + 
+    xlab("metric quantile") + 
+    geom_boxplot(width=0.1) # + theme_minimal() + theme(legend.position = "none")
 ggsave("example.jpg")
 
 
