7cd69b403242eb10bec5c1b15ec0bf13800bcfc4
Diogo Melo <diogro@gmail.com>
2022-03-04 14:47:51 -0500

    go plots

--- analysis/results_statistics.R
+++ analysis/results_statistics.R
@@ -3,49 +3,6 @@ source(here::here("functions.R"))
 library(here)
 library(sjmisc)
 library(pryr)
-
-# %%
-
-# %%
-# Read in the data for each chunk in parallel if needed. 
-file_paths <- list.files(path = here::here("snakemake/Rdatas/residuals/"), 
-                         pattern = "\\.rds", full.names = TRUE)
-file_names <-  gsub(pattern = "\\.rds$", replacement = "", x = basename(file_paths))
-
-library(plyr)
-library(doMC)
-registerDoMC(64)
-data_list <- llply(file_paths, readRDS, .parallel = TRUE)
-
-names(data_list) <- file_names
-
-residuals_noout <- list()
-for (name in names(data_list)){
-    residuals_noout[[name]] <- as.data.frame(data_list[[name]]$residuals_noOut) %>% rotate_df()
-}
-
-# %%
-# %%
-
-
-residuals_df <-bind_rows(residuals_noout,.id = 'source')
-dim(residuals_df)
-n_dsets = length(data_list)
-source = residuals_df$source
-
-residuals_array = as.matrix(residuals_df[,-1])
-residuals_array <- Rfast::transpose(residuals_array)
-
-
-residuals_df <- residuals_df[1:10, 1:10] %>% rotate_df()
-residuals_df$genes = rownames(residuals_df)
-summarized_df <- residuals_df
-# summarized_df = purrr::reduce(summarized_list, dplyr::full_join, by = "Genes")
-# colnames(summarized_df)[-1] = names(results_list)
-# x = summarized_df[1,-(1:2)]  
-max_missingness =0.5
-rowmask = apply(summarized_df[,-c(1:2)], 1, function(x) sum(is.na(x))/n_dsets < max_missingness) 
-summarized_df = summarized_df[rowmask,]
 # %%
 
 # %%
@@ -85,10 +42,10 @@ local_go_upper = enrichGO(gene  = upper_quantiles[[metric]],
                           pvalueCutoff  = 0.01,
                           qvalueCutoff  = 0.05,
                           readable      = TRUE)
-png(here::here("data/plots/local_go_upper.png"), height = 3840, width = 2160)
-barplot(local_go_upper, showCategory=100) 
-dev.off()
 
+pw_upper <- pairwise_termsim(local_go_upper) 
+p1 <- emapplot(pw_upper, showCategory = 20) + theme(legend.position = "none")
+p1$data$color = mean(p1$data$color)
 local_go_lower = enrichGO(gene  = lower_quantiles[[metric]],
                           universe = rank_df$Gene,
                           OrgDb         = org.Hs.eg.db,
@@ -98,35 +55,12 @@ local_go_lower = enrichGO(gene  = lower_quantiles[[metric]],
                           pvalueCutoff  = 0.01,
                           qvalueCutoff  = 0.05,
                           readable      = TRUE)
-png(here::here("data/plots/local_go_lower.png"), height = 3840, width = 2160)
-barplot(local_go_lower , showCategory=100) 
-dev.off()
+pw_lower <- pairwise_termsim(local_go_lower) 
+p2 <- emapplot(pw_lower, showCategory = 20) + theme(legend.position = "none")
+p2$data$color = mean(p1$data$color)
+save_plot(here::here("data/plots/local_go_lower.png"), plot_grid(p1, p2, labels = c("High variation", "Low variation")), base_height = 7, base_asp = 1.5, ncol = 2)
 # %%
 
 
 
 
-# %%
-file_paths <- list.files(path = here::here("snakemake/Rdatas/residuals/"), 
-                         pattern = "\\.rds", full.names = TRUE)
-file_names <-  gsub(pattern = "\\.rds$", replacement = "", x = basename(file_paths))
-
-library(plyr)
-library(doMC)
-registerDoMC(64)
-data_list <- llply(file_paths, readRDS, .parallel = TRUE)
-
-names(data_list) <- file_names
-
-# %%
-
-# %%
-library("biomaRt")
-# listMarts()
-mart = useMart(biomart="ENSEMBL_MART_ENSEMBL", host="grch37.ensembl.org", path="/biomart/martservice" ,dataset="hsapiens_gene_ensembl")
-
-
-
-# %%
-
-metrics <- readRDS("snakemake/Rdatas/gene_metrics.RDS")
