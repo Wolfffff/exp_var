21d6045f68fc75a50b1f0a6157d7c9493111cd9b
Wolfffff <scott.w.wolf1@gmail.com>
2022-04-05 14:24:36 -0400

    Working traversal of GO terms

--- analysis/go_traversal.R
+++ analysis/go_traversal.R
@@ -21,35 +21,31 @@ ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
 
 # %%
 call_go <- function(id) {
-  print(id)
-  gene.data <- getBM(attributes=c('hgnc_symbol', 'ensembl_gene_id', 'go_id', "name_1006", "namespace_1003"),
-                filters = 'go', values = id, mart = ensembl)
-  return(gene.data)
+  gene.data = mget(c(id),org.Hs.egGO2ALLEGS)
+  entrezgene = unlist(gene.data)
+  genes <- getBM(filters="entrezgene_id", attributes=c("ensembl_gene_id","entrezgene_id"), values=entrezgene, mart=ensembl)
+  return(genes)
 }
 
 
 library(GO.db)
 library(biomaRt)
-get_go_genes <- function(go_id) {
-  print(go_id)
-  go_ids <- c(GOBPOFFSPRING[[go_id]], go_id)
-  all <- lapply(go_ids, call_go)
-  return(all)
-}
 
-go_gene_groups <- lapply(go_terms, get_go_genes)
+go_gene_groups <- lapply(go_terms, call_go)
 names(go_gene_groups) <- go_terms
-
-go_gene_groups_flat <- lapply(go_gene_groups, rbindlist)
-save.image(file='go_traversal_1.RData')
 go_gene_overlapping <- list()
 for (term in go_terms) {
-  go_gene_overlap <- go_gene_groups_flat[[term]][go_gene_groups_flat[[term]]$ensembl_gene_id %in% rank_df[, 1],]
+  go_gene_overlap <- go_gene_groups[[term]][go_gene_groups[[term]]$ensembl_gene_id %in% rank_df$Gene,]
   go_gene_overlap <- go_gene_overlap[!duplicated(go_gene_overlap$ensembl_gene_id),]
   go_gene_overlapping[[term]] <- go_gene_overlap
 }
-save.image(file='go_traversal_2.RData')
+
+lapply(go_gene_overlapping,dim)
+save.image(file='go_traversal.RData')
 
 # %%
 
-lapply(go_gene_overlapping,dim)
+
+
+
+
