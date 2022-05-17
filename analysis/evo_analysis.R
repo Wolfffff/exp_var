18c0bfe8b2367d5ec9aa829ef7e449cdd2c55169
Diogo Melo <diogro@gmail.com>
2022-05-17 13:45:46 -0400

    some refactoring

--- analysis/evo_analysis.R
+++ analysis/evo_analysis.R
@@ -1,6 +1,6 @@
 # File for evo analysis including mapping PopHuman
 # %%
-metric_df = readRDS(here::here("snakemake/Rdatas/mean_variance.RDS"))
+source(here::here("functions.R"))
 rank_df = read.csv(here::here("data/pca_ranks.csv"), header = TRUE)[, -1]
 imkt_df = read.csv(here::here("data/annotation/imkt_results.csv"))
 imkt_df$gene = str_split_fixed(imkt_df$gene,'\\.',Inf)[,1]
@@ -21,7 +21,8 @@ cor_mat = cor(merged_filtered[,c(-1)],method="spearman")
 # Worth looking into -- FDR corrected here.
 cor_test_mat = corr.test(merged_filtered[,-1],method="spearman",adjust="fdr")
 png(here::here("data/plots/SpearmanCorrelations/corr_plot_with_pvals.png"), height = 2160, width = 2160)
-corrplot(cor_mat,method='ellipse')
+diag(cor_mat) = NA
+corrplot.mixed(cor_mat, upper='ellipse')
 mtext("Spearman Correlation Plot - Numbers are p-vals", at=3.5, line=-0.5, cex=4)
 pos <- expand.grid(1:ncol(cor_test_mat$p), ncol(cor_test_mat$p):1)
 text(pos, p_format(cor_test_mat$p))
@@ -31,18 +32,6 @@ merged_filtered$KaKs <- merged_filtered$Divergence.metrics.Ka/merged_filtered$Di
 merged_filtered <- merged_filtered[is.numeric(merged_filtered$KaKs),]
 
 # %%
-
-# %%
-mod = lm(sd ~ scale(Divergence.metrics.omega) + scale(mean), merged_filtered)
-mod.summary = summary(mod)
-mod.summary
-
-p <- ggplot(merged_filtered, aes(x = rank(pi), y = sd)) + geom_point(alpha = 0.1)
-save_plot("test.png", p)
-quantile_violin_plot( merged_filtered$pi  ,merged_filtered$sd,ntiles=10) + ylab("SD Rank") + xlab("pi value quantile") + geom_boxplot(width=0.1) + stat_summary(fun = "mean", geom = "point", color = "red")
-ggsave(here::here("data/plots/violin_plots/pi.jpg"), width = 18, height = 6, units = "in", dpi = 300) 
-# %%
-
 
 # %%
 # pi vals
@@ -60,8 +49,8 @@ seqlevels(pi_ceu)<- sub('chr','',seqlevels(pi_ceu))
 genes_with_pi <- mergeByOverlaps(gene_annotations,pi_ceu)
 results_df = data.frame(gene = character(),pi = numeric())
 for(gene in unique(genes_with_pi$gene_id)){
-    results = c(gene, mean(genes_with_pi[genes_with_pi$gene_id == gene,]$score))
-    results_df[nrow(results_df)+1,] = results
+    results = data.frame(gene = gene, pi = mean(genes_with_pi[genes_with_pi$gene_id == gene,]$score))
+    results_df = rbind(results_df, results)
 }
 
 write.csv(results_df, here::here("data/annotation/pi_ceu_results.csv"),row.names = F)
